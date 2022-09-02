fe63212a76cbfe955ff697ac9098968b3d7c87d6
Ahmed Alghali <95593096+A7med7x7@users.noreply.github.com>
2022-09-02 12:23:18 +0200

    Add files via upload
    calculate the distance from the origin point (0,0) and print it

---
+++ disctance from origin.py
@@ -0,0 +1,7 @@
+x1 = int(input("what is your first point"))
+y1 = int(input("what is your second point"))
+x0 = 0
+y0 = 0
+
+distance = abs((((x1 - x0)**2))) + (((y1-y0)**2)**0.5)
+print ("distance from the origin is: ",distance)
